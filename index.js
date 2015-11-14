6e81f325f60c302b036051f34b4af4750a6a728c
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2015-11-13 23:23:12 -0600

    Focus and Hover

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 new Vue({
   el: '#vue-demo',
   data: {
+    newItem: '',
     left: [
       { text: 'First Item' },
       { text: 'Second Item' },
@@ -13,13 +14,13 @@ new Vue({
     ]
   },
   methods: {
-    moveToRight: function (action) {
-      this.left.$remove(action);
-      this.right.push(action);
+    moveToRight: function (item) {
+      this.left.$remove(item);
+      this.right.push(item);
     },
-    moveToLeft: function (action) {
-      this.right.$remove(action);
-      this.left.push(action);
+    moveToLeft: function (item) {
+      this.right.$remove(item);
+      this.left.push(item);
     },
     moveAllToRight: function () {
       this.right = this.right.concat(this.left);
@@ -28,6 +29,14 @@ new Vue({
     moveAllToLeft: function () {
       this.left = this.left.concat(this.right);
       this.right = [];
+    },
+    addItem: function (list) {
+      this[list].push( { text: this.newItem });
+      this.newItem = '';
+      this.$els.item_input.focus();
+    },
+    removeItem: function (item, from) {
+      this[from].$remove(item);
     }
   }
 });
